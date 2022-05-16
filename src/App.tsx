e1f08c112e18aef6c3cace5bcdddcb118366ca57
JonnyNiks <jonnyniks@gmail.com>
2022-05-16 15:36:21 +0300

    feat: add changing theme by click on button

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button } from './components/Button'
 import { ReactComponent as BookmarkIcon } from './assets/images/bookmark.svg'
 import { ReactComponent as LikeIcon } from './assets/images/like.svg'
 import { ReactComponent as DislikeIcon } from './assets/images/dislike.svg'
 
 function App() {
+  const [theme, setTheme] = useState('dark')
   const handleButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     console.log(event.target)
   }
 
+  const toggleTheme = () => {
+    const newTheme = theme === 'dark' ? 'light' : 'dark'
+    setTheme(newTheme)
+  }
+
   return (
-    <div className="App">
-      <Button
-        text="Primary"
-        disabled
-        className="primary"
-        onClick={handleButtonClick}
-      />
+    <div className={`App theme--${theme}`}>
+      <Button text="Primary" className="primary" onClick={toggleTheme} />
       <Button
         text="Secondary"
         className="secondary"
