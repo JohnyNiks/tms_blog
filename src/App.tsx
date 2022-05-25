67061a89f0280ead9e23939261e46010d15b58f5
JonnyNiks <jonnyniks@gmail.com>
2022-05-25 22:49:04 +0300

    feat: add possibility to get post in App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,32 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Button } from './components/Button'
-import { Input } from './components/Input'
 
 import { useTheme } from './features/theme'
 
-import { useAppDispatch } from './redux/hooks'
-import { signUp } from './features/auth'
+import { useAppDispatch, useAppSelector } from './redux/hooks'
+import { getPost } from './features/posts'
 
 function App() {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [confirmedPassword, setConfirmedPassword] = useState('')
-
   const { theme, toggleTheme } = useTheme()
-
-  const onNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setName(event.target.value)
-  }
-
-  const onEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(event.target.value)
-  }
-
-  const onPasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(event.target.value)
-  }
-
-  const onConfirmedPasswordChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setConfirmedPassword(event.target.value)
-  }
-
+  const post = useAppSelector(state => state.posts.post)
   const dispatch = useAppDispatch()
 
-  const submitForm = (event: React.MouseEvent<HTMLButtonElement>): void => {
-    event.preventDefault()
-    const formData = {
-      name,
-      email,
-      password,
-    }
-    dispatch(signUp())
+  const fetchPost = () => {
+    dispatch(getPost(12))
   }
 
   return (
     <div className={`App theme--${theme}`}>
       <Button text="Primary" className="primary" onClick={toggleTheme} />
-      <form>
-        <Input title="Name" value={name} onChange={onNameChange} />
-        <Input title="Email" value={email} onChange={onEmailChange} />
-        <Input
-          type="password"
-          title="Password"
-          value={password}
-          onChange={onPasswordChange}
-        />
-        <Input
-          type="password"
-          title="Confirm password"
-          value={confirmedPassword}
-          onChange={onConfirmedPasswordChange}
-        />
-        <Button text="Sign Up" className="primary" onClick={submitForm} />
-      </form>
+      <Button text="Primary" className="primary" onClick={fetchPost} />
+      {post && (
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <span>ID: {post.id}</span>
+          <span>Date: {post.date}</span>
+          <span>Text: {post.text}</span>
+          <span>Title: {post.title}</span>
+        </div>
+      )}
     </div>
   )
 }
